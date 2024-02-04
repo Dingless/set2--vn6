e962beb28123bca74d83f35e27ab655a2f528d08
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-02-04 17:49:15 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using System.Collections.Generic;// jag använde system.generic.collections så att jag kan använda mig av listor.
+
+List<string> cities= new List<string>(); // detta är en den tomma listan som heter cities som kan hålla strings
